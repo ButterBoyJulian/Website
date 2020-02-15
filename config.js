20506f2a2549f3aa516d53a2fc7d8d574777e1a8
ButterBoyJulian <34288129+ButterBoyJulian@users.noreply.github.com>
2020-02-14 21:06:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,6 @@ var config = {
   'COPYRIGHT' : 'Julian Network 2020',
   'FOOTER' : 'Hope you Enjoyed the Website!'
 }
+
+document.getElementById("copyright").innerHTML = "©" + config.COPYRIGHT;
+document.getElementById("footer").innerHTML = config.FOOTER;
