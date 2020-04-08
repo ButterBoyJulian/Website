e0ab10e395d1ef61d21abaeabc257b4b0dfda53d
ButterBoyJulian <34288129+ButterBoyJulian@users.noreply.github.com>
2020-04-08 15:18:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,23 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('node_modules/'));
+// p5js
+app.use(express.static('public'));
+app.use(express.static('node_modules/p5/lib'));
+app.use(express.static('node_modules/p5/lib/addons'));
 
 var listener = app.listen(PORT, function () {
   console.log('The server is listening on port ' + listener.address().port);
 });
+
+const socket = require('socket.io');
+const io = socket(listener);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+  console.log("New Connection: " + socket.id);
+}
 
 //require('./src/(File).js');
 
