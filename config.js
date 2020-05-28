84e6bb0db27099a337bb46f49d7d2c301cfe4a18
ButterBoyJulian <34288129+ButterBoyJulian@users.noreply.github.com>
2020-05-28 12:29:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,12 @@ var config = {
   "FOOTER": "Hope you Enjoyed the Website!"
 }
 
+// Bottom of Site
 document.getElementById("copyright").innerHTML = "©" + config.COPYRIGHT;
 document.getElementById("footer").innerHTML = config.FOOTER;
+
+// Site Banner
+var img = document.createElement("img");
+img.src = "images/background-cropped.png";
+var src = document.getElementById("site-banner");
+src.appendChild(img);
