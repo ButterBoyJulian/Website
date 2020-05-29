63b3d65490132ded503bcd39ab05f52721544c9b
ButterBoyJulian <34288129+ButterBoyJulian@users.noreply.github.com>
2020-05-28 22:02:02 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = require('settings.json');
 
 // Bottom of Site
-document.getElementById("copyright").innerHTML = "©" + config.COPYRIGHT;
-document.getElementById("footer").innerHTML = config.FOOTER;
+document.getElementById("footer").innerHTML = config.siteBottom.footer;
+document.getElementById("copyright").innerHTML = "©" + config.siteBottom.copyright;
 
 // Site Banner
 //document.getElementById("banner").style.backgroundImage = "url('https://julian-network.com/images/background-cropped.png')";
